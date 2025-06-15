265554ada599086689bb3cca1ecbfbd406b1eba4
DanielAlecode <danielalexisd8@gmail.com>
2025-06-14 20:35:23 -0600

    Project Set-up

---
+++ ListsChurn/src/main/java/com/mycompany/listschurn/ListsChurn.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.listschurn;
+
+/**
+ *
+ * @author daniel
+ */
+public class ListsChurn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
